a5341f4659b402d265b9c55c854c523c4237d013
gvlfm78 <gvlfm78@gmail.com>
2016-09-04 17:29:23 +0200

    Still doing things

---
+++ BukkitHotels/src/gvlfm78/plugin/Hotels/events/RoomCreateEvent.java
@@ -0,0 +1,25 @@
+package gvlfm78.plugin.Hotels.events;
+
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+import gvlfm78.plugin.Hotels.Room;
+
+public class RoomCreateEvent extends Event{
+
+	private static final HandlerList handlers = new HandlerList();
+	
+	private Room room;
+
+	public HandlerList getHandlers() {
+		return handlers;
+	}
+	
+	public RoomCreateEvent(Room room){
+		this.room = room;
+	}
+	public Room getRoom(){
+		return room;
+	}
+	
+}
