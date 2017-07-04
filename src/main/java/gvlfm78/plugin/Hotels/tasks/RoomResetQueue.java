37ce41ce2334fb59124a2bdb22aac32058d3c7cd
gvlfm78 <gvlfm78@gmail.com>
2017-07-04 19:29:39 +0100

    Fixed error in RoomResetQueue

--- src/main/java/gvlfm78/plugin/Hotels/tasks/RoomResetQueue.java
+++ src/main/java/gvlfm78/plugin/Hotels/tasks/RoomResetQueue.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package gvlfm78.plugin.Hotels.tasks;
 
@@ -7,8 +7,8 @@ import com.sk89q.worldedit.WorldEditException;
 import com.sk89q.worldedit.world.DataException;
 import gvlfm78.plugin.Hotels.HotelsMain;
 import gvlfm78.plugin.Hotels.Room;
+import gvlfm78.plugin.Hotels.handlers.HTConfigHandler;
 import gvlfm78.plugin.Hotels.managers.Mes;
-import org.bukkit.Bukkit;
 
 import java.io.IOException;
 
@@ -17,28 +17,28 @@ import java.io.IOException;
  */
 public class RoomResetQueue {
 
-	private static HotelsMain PLUGIN = HotelsMain.getHotels();
 	private static long lastAdding;
-	
+
 	public static void add(final Room room){
-		
-		long delay = PLUGIN.getConfig().getLong("roomResetDelay", 5L);
+
+		long delay = HTConfigHandler.getconfigYML().getLong("roomResetDelay", 5L);
 		long currentTime = System.currentTimeMillis();
-		
+
 		//This makes it so there is a constant minimum time difference between tasks
 		long secondsUntilLastTaskIsExecuted = delay - (currentTime - lastAdding) / 1000;
 
 		if(secondsUntilLastTaskIsExecuted < 0) secondsUntilLastTaskIsExecuted = 0;
-		
-		Bukkit.getScheduler().runTaskLater(PLUGIN, () -> {
-            try {
-                Mes.debug("Resetting room " + room.getNum() + " of Hotel " + room.getHotel().getName());
-                room.reset();
-            } catch (IOException | WorldEditException | DataException e) {
-                e.printStackTrace();
-            }
-        }, (20 * (delay + secondsUntilLastTaskIsExecuted)) );
 
+		HotelsMain.runTaskLater(20 * (delay + secondsUntilLastTaskIsExecuted),
+				() -> {
+					try {
+						Mes.debug("Resetting room " + room.getNum() + " of Hotel " + room.getHotel().getName());
+						room.reset();
+					} catch (IOException | WorldEditException | DataException e) {
+						e.printStackTrace();
+					}
+				}
+		);
 		lastAdding = currentTime;
 	}
 }
