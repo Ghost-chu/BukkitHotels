9756056efab68d82b6dafe125e532a271a90b51b
gvlfm78 <gvlfm78@gmail.com>
2017-07-09 12:06:34 +0100

    Hotel helper functionality & made messages fall back to default if null

--- src/main/java/gvlfm78/plugin/Hotels/exceptions/FriendNotFoundException.java
+++ src/main/java/gvlfm78/plugin/Hotels/exceptions/FriendNotFoundException.java
@@ -1,5 +1,8 @@
 package gvlfm78.plugin.Hotels.exceptions;
 
+/**
+ * Thrown when friend or helper cannot be found
+ */
 public class FriendNotFoundException extends HotelsException {
 	
 	private static final long serialVersionUID = 1L;	
